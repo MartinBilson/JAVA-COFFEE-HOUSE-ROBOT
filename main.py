96d3f25c75414ee6f8a752c838264ba713392513
MartBilson254 <109283741+MartinBilson@users.noreply.github.com>
2022-07-21 19:46:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hello, welcome to Java coffe shop!!!!!!")
+
+name = input("What is your name?\n\n\n")
+
+print("Hello "+ name +" , thank you soo much for shopping with us\n\n\n")
+
+
+menu = ("Our 1st World alkaline coffee: black coffee, lattee, capuccino, sugar free, original\n\n\n")
+
+print(name + ",what would you like from our menu today? here is what we are serving.\n\n\n" +menu)
+
+
+order = input()
+
+print("sound good "+ name +" we will have that " + order + "  ready for you in a moment.")
+
+
+
